f380811e03ddd4d4b5b735fc90827413d4f214d3
GavinDes <gavin17.business@gmail.com>
2017-10-22 14:29:35 -0500

    Added Chase.java

---
+++ assignments/Chase.java
@@ -0,0 +1,22 @@
+/* 
+Virtual Assignment Three: Chase
+
+Objective:
+Fix Chase's code, so that it outputs variable "phrase".
+*/
+
+
+import java.Scanner;
+
+public class Chase
+{
+
+   public static void main(String[] args)
+   {
+      System.out.print("Enter a phrase: ");
+      Scanner user_input = Scanner(system.in);
+      string phrase = user_input.nextLine();
+      System.out.print(phrase)
+   }
+
+}
