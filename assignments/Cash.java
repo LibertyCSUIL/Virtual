cd90b231cce4382c8b9d2bc2d135cd81b14698a7
GavinDes <gavin17.business@gmail.com>
2017-10-26 16:31:24 -0500

    Added cash.java to assignments directory.

---
+++ assignments/Cash.java
@@ -0,0 +1,27 @@
+/* 
+Virtual Assignment Four: Cash
+
+Objective:
+Make a program that outputs "You have enough cash."
+if the user inputs anything 5.00 or above, and outputs
+"You need more cash!" if the user inputs anything below
+5.00 . 
+
+Requirements:
+You must use doubles.
+*/
+
+
+import java.util.Scanner;
+
+public class Cash
+{
+
+   public static void main(String[] args)
+   {
+      
+      
+      
+   }
+
+}
