0a756488bf249dc9a2877a672ae70c9894b02fe4
GavinDes <gavin17.business@gmail.com>
2017-10-28 06:22:58 -0500

    Added Casey.java to assignments

---
+++ assignments/Casey.java
@@ -0,0 +1,30 @@
+/* 
+Virtual Assignment Five: Casey
+
+Objective:
+Fix Casey's code, so that it outputs
+"x * x is less than or equal to 100"
+when x * x is less than or equal to 100
+and outputs "x * x is greater than 100"
+when x * x is greater than 100.
+*/
+
+
+
+
+public class Casey
+{
+
+   public static void main(String[] args)
+   {
+      System.out.print("x equals: ");
+      Scanner input = new Scanner(System.in);
+      long x = input.nextInt();
+      if(x*X > 100)
+         System.out.println("x * x is less than or equal to 100");
+      else
+         System.out.println("x * x is greater than 100");
+      
+   }
+
+}
