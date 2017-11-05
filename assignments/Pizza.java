865c5cb1daacf11d32795ee799bb343bb9f8711a
GavinDes <gavin17.business@gmail.com>
2017-11-05 14:07:27 -0600

    Added Pizza.java to assignments

---
+++ assignments/Pizza.java
@@ -0,0 +1,32 @@
+/*
+Programmer:  
+
+Virtual Program Assignment Five: Pizza
+
+Objective:
+Make a program that outputs the number of pizza
+slices you'll need to cut so that every person
+gets one slice of pizza; the user will input how 
+many people there are. The maximum number of 
+pizza slices are ten, anything above ten outputs:
+"Not enough pizza :(".
+
+Requirements:
+1. You can only use longs
+2. You can only use switch statements
+as a conditional.
+*/
+
+import java.util.Scanner;
+
+public class Pizza
+{
+
+   public static void main(String[] args)
+   {
+      
+      
+      
+   }
+
+}
