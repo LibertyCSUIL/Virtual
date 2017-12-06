242ccd898548454ab474a83ec7ddcaf9ec9aeed6
GavinDes <gavin17.business@gmail.com>
2017-12-05 22:53:16 -0600

    Added Connor.java

---
+++ assignments/Connor.java
@@ -0,0 +1,29 @@
+/*
+Programmer: 
+
+Virtual Assignment Six: Connor
+
+Objective:
+Fix Connor's code, so that it outputs the numbers 0
+through 9 on each line.
+*/
+
+
+public class Connor
+{
+
+   public static void main(String[] args)
+   {
+      int anArray{} = new int[??];
+      for(int i = 0; i < 10; ++i)
+      {
+         anArray[??] = i;
+      }
+
+      for(int t: ??)
+      {
+         System.out.print(anArray[t]);
+      }
+   }
+
+}
